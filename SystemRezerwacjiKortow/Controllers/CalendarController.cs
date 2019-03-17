2a334fb045fabf788322e47a325f3c9158dd906a
kambudka <kambudka@wp.pl>
2019-03-18 00:01:11 +0100

    Alpha

--- SystemRezerwacjiKortow/Controllers/CalendarController.cs
+++ SystemRezerwacjiKortow/Controllers/CalendarController.cs
@@ -3,15 +3,80 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SystemRezerwacjiKortow.Models;
+using SystemRezerwacjiKortow.Database;
+using System.Runtime;
 
 namespace SystemRezerwacjiKortow.Controllers
 {
     public class CalendarController : Controller
     {
+        public List<Reservation> ReservationList;
         // GET: Calendar
         public ActionResult Index()
         {
+
             return View();
+        }
+
+        public int GetUserID()
+        {
+            return 0;
+        }
+
+        public JsonResult GetEvents(string paramstart, string paramend)
+        {
+            ReservationList = new List<Reservation>();
+            ReservationList = SqlReservation.GetReservations(13);
+            List<Event> list = new List<Event>();
+            Event newevent;
+            DateTime datetime = DateTime.Now;
+
+            foreach (Reservation reservation in ReservationList)
+            {
+                newevent = new Event();
+                //newevent.id = reservation.ReservationID;
+                newevent.title = reservation.CourtID.ToString();
+                newevent.start = ConvertFromDateToString(reservation.DateFrom);
+                newevent.end = ConvertFromDateToString(reservation.DateTo);
+                list.Add(newevent);
+            }
+
+            var events = list.ToArray();
+            return new JsonResult { Data = events, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+        }
+
+        [HttpPost]
+        public JsonResult SaveEvent(Event e)
+        {
+            var status = false;
+              /*  if (e.EventID > 0)
+                {
+                    //Update the event
+                    var v = dc.Events.Where(a => a.EventID == e.EventID).FirstOrDefault();
+                    if (v != null)
+                    {
+                        v.Subject = e.Subject;
+                        v.Start = e.Start;
+                        v.End = e.End;
+                        v.Description = e.Description;
+                        v.IsFullDay = e.IsFullDay;
+                        v.ThemeColor = e.ThemeColor;
+                    }
+                }
+                else
+                {
+                    dc.Events.Add(e);
+                }
+                dc.SaveChanges();*/
+                status = true;
+            return new JsonResult { Data = new { status = status } };
+        }
+
+        public static string ConvertFromDateToString(DateTime timestamp)
+        {
+            string result = timestamp.ToString("yyyy-MM-ddTHH:mm:ss");
+            return result;
         }
     }
 }
