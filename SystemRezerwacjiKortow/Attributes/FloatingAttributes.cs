3a73ef2ebfcefa08112573db3c7b1a7f935e1e27
Mateusz Nawrocki <nawrockimateusz96@gmail.com>
2019-03-19 02:07:08 +0100

    Poprawki

---
+++ SystemRezerwacjiKortow/Attributes/FloatingAttributes.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace SystemRezerwacjiKortow.Attributes
+{
+    public class FloatingAttributes:ValidationAttribute
+    {
+        public FloatingAttributes():base("{0} ma niepoprawny format") { }
+        
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if(value==null) return ValidationResult.Success;
+            decimal floatValue;
+            if(Decimal.TryParse(value.ToString(),out floatValue))
+            return ValidationResult.Success;
+            var errorMessage = FormatErrorMessage((validationContext.DisplayName));
+            return new ValidationResult(errorMessage);
+        }
+    }
+}
---
+++ SystemRezerwacjiKortow/ViewModels/UserCustomerViewModel.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using SystemRezerwacjiKortow.Resources;
+
+namespace SystemRezerwacjiKortow.ViewModels
+{
+    public class UserCustomerViewModel
+    {
+
+        public int UserID { get; set; }
+        public int CustomerID { get; set; }
+
+        [Display(Name = "CompanyName", ResourceType = typeof(Texts))]
+        public string CompanyName { get; set; }
+
+        [Display(Name = "City", ResourceType = typeof(Texts))]
+        [StringLength(50)]
+        [Required(AllowEmptyStrings = false, ErrorMessageResourceType = typeof(Texts), ErrorMessageResourceName = "EnterYourCity")]
+        public string City { get; set; }
+
+        [Display(Name = "Street", ResourceType = typeof(Texts))]
+        [StringLength(50)]
+        [Required(AllowEmptyStrings = false, ErrorMessageResourceType = typeof(Texts), ErrorMessageResourceName = "EnterYourStreet")]
+        public string Street { get; set; }
+
+        [Display(Name = "PostalCode", ResourceType = typeof(Texts))]
+        [StringLength(6)]
+        [Required(AllowEmptyStrings = false, ErrorMessageResourceType = typeof(Texts), ErrorMessageResourceName = "EnterYourPostalCode")]
+        public string ZipCode { get; set; }
+
+        [Display(Name = "Discount", ResourceType = typeof(Texts))]
+        [DataType(DataType.Currency)]
+        public decimal DiscountValue { get; set; }
+
+        [Display(Name = "CanReserve", ResourceType = typeof(Texts))]
+        public bool CanReserve { get; set; }
+
+
+        [Display(Name = "Name", ResourceType = typeof(Texts))]
+        [Required(AllowEmptyStrings = false, ErrorMessageResourceType = typeof(Texts), ErrorMessageResourceName = "EnterYourName")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Surname", ResourceType = typeof(Texts))]
+        [Required(AllowEmptyStrings = false, ErrorMessageResourceType = typeof(Texts), ErrorMessageResourceName = "EnterYourSurname")]
+        public string Surname { get; set; }
+
+        [Display(Name = "DateOfBirth", ResourceType = typeof(Texts))]
+        [Required(AllowEmptyStrings = false, ErrorMessageResourceType = typeof(Texts), ErrorMessageResourceName = "EnterYourDateOfBirth")]
+        [DataType(DataType.Date)]
+        //[DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MM/yyyy}")]
+        public DateTime DateOfBirth { get; set; }
+
+
+
+
+    }
+}
