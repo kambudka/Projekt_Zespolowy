20f6174add8883b1156e01ea856b5fd6deff4b5d
Mateusz Nawrocki <nawrockimateusz96@gmail.com>
2019-04-01 22:02:25 +0200

    Wysyłanie maila z rezerwacją

--- SystemRezerwacjiKortow/Controllers/AdminController.cs
+++ SystemRezerwacjiKortow/Controllers/AdminController.cs
@@ -2,6 +2,8 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Web;
 using System.Web.Mvc;
 using SystemRezerwacjiKortow.Database;
@@ -56,14 +58,30 @@ namespace SystemRezerwacjiKortow.Controllers
                 SqlReservation.AcceptReservation(id, true);
                 SqlReservation.MakePayment(id);
                 ViewBag.MyErrorMessage = true;
+                SendCourtReservationMail(SqlReservation.GetReservation(id));
                 return RedirectToAction("WaitingReservations");
             }
             catch
             {
                 return RedirectToAction("WaitingReservations");
             }
+        }
 
-            
+        public void SendCourtReservationMail(Reservation reservation)
+        {
+            var subject = "Potwierdzenie rezerwacji nr " + reservation.ReservationID;
+            var body = "<p><img src=\"https://student.labranet.jamk.fi/~H3188/wp_harjoitustyo/wordpress/wp-content/gallery-bank/gallery-uploads/o_1a68kjem61o38eikheljsibvhi.jpg\" width=\"600\" height=\"100\" /></p>"+
+            "<p><font size=\"4\"> Dziękujemy za skorzystanie z usług Serwisu internetowej rezerwacji kortów.Poniżej przesyłamy podsumowanie Twojej transakcji rezerwacji.</font></p>" +
+                     "<p> &nbsp; &nbsp; ID transakcji: <strong>"+reservation.ReservationID+ "</strong></p>"+
+                        "<p> &nbsp; &nbsp; Numer kortu: "+SqlCourt.GetCourt(reservation.CourtID).CourtNumber+"</p>"+
+                           "<p> &nbsp; &nbsp; Data rozpoczęcia rezerwacji: <strong>"+reservation.DateFrom.ToString()+"</strong></p>"+
+                              "<p> &nbsp; &nbsp; Data zakończenia rezerwacji: <strong>" + reservation.DateTo.ToString()+ "</strong></p>" +
+                                 "<p> &nbsp; &nbsp; &nbsp;<strong> ul.Zwierzyniecka 45A 15 - 333, Bialystok </strong></p>"+
+                                       "<p><span style = \"color: #999999;\"> Rezerwację należy opłacic w kasie najpóźniej 10 minut przed rozpoczęciem rezerwacji. Aby odwołać rezerwację skontaktuj się z</span> &nbsp;<a href = \"#\" >twojekorty@gmail.com </a></p> ";
+
+            var user = SqlUser.GetUserByID(reservation.UserID);
+
+            Email.SendEmail(subject, body,user.Email,user.FirstName) ;
         }
 
         public ActionResult WaitingReservations()
