5ab78c923f5e1dece54cbc893f5c0f5a7d6c65e0
GOSIACZEK1801 Kuczkowska <gosiakuc18@o2.pl>
2019-04-07 16:07:24 +0200

    Poprawione Turnieje (Index + Details)

--- SystemRezerwacjiKortow/Controllers/ContestsController.cs
+++ SystemRezerwacjiKortow/Controllers/ContestsController.cs
@@ -3,15 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SystemRezerwacjiKortow.Models;
+using SystemRezerwacjiKortow.Database;
+using System.Net;
 
 namespace SystemRezerwacjiKortow.Controllers
 {
     public class ContestsController : Controller
     {
-        // GET: Contests
+
+        List<Contest> list = SqlContest.GetContests(0); // UserID 0 zwraca wszystkie turnieje
+
+        // GET: Contest
         public ActionResult Index()
         {
-            return View();
+            return View(list);
         }
+
+        // GET: Contest/Details/5
+
+        public ActionResult Details(int id)
+        {
+            Contest contest = SqlContest.GetContest(id);
+            return View(contest);
+        }
+
+
     }
 }
