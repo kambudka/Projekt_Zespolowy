2579b807dd394d32f55b0dc9718a3d5d5e96df83
Krzysztof Kiełczewski <kkielek97@gmail.com>
2019-05-07 09:25:21 +0200

    prt 1

--- SystemRezerwacjiKortow/Controllers/CurrencyController.cs
+++ SystemRezerwacjiKortow/Controllers/CurrencyController.cs
@@ -1,17 +1,38 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using SystemRezerwacjiKortow.Models;
 
 namespace SystemRezerwacjiKortow.Controllers
 {
     public class CurrencyController : Controller
     {
+        private CurrencyService service = new CurrencyService();
+
         // GET: Currency
-        public ActionResult Index()
+        public async Task<ActionResult> Index()
         {
+            ViewBag.Currencies = await service.GetCurrenciesAsync();
             return View();
+        }
+    }
+
+    public class CurrencyService {
+        string url = "http://api.nbp.pl/api/exchangerates/tables/a/?format=json";
+
+        public async Task<List<Currency>> GetCurrenciesAsync()
+        {
+            using (HttpClient httpClient = new HttpClient())
+            {
+                string json = await httpClient.GetStringAsync(url);
+                List <Currency> currenciesList = JsonConvert.DeserializeObject<List<Currency>>(json);
+                return currenciesList;
+            }
         }
     }
 }
